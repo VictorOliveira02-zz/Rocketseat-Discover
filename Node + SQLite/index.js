956dfae2dd08e70b1324b8819fc2722e60a33bff
VictorOliveira02 <victoralvees17@gmail.com>
2022-02-14 21:14:29 -0300

    make CRUD functions

--- Node + SQLite/index.js
+++ Node + SQLite/index.js
@@ -1,14 +1,30 @@
-import { createTable } from './src/Controller/aluno.js';
+import { createStudentTable, insertStudent, updateStudent, allStudents, deleteStudent } from './src/Controller/aluno.js';
 
 import express from "express";
 const app = express();
 app.use(express.json());
 
-createTable();
+createStudentTable();
 
-app.get('/', (req, res) => {
-
+app.get('/', async (req, res) => {
+    let students = await allStudents()
+    res.json(students);
 });
+
+app.post('/alumni', (req, res) => {
+    insertStudent(req.body)
+    res.send('Success Insert');
+})
+
+app.put('/alumni/update', (req, res) => {
+    updateStudent(req.body)
+    res.send('Success Update');
+})
+
+app.delete('/alumni/delete/:id', (req, res) => {
+    deleteStudent(req.params.id)
+    res.send('Success Delete');
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- Node + SQLite/src/Controller/aluno.js
+++ Node + SQLite/src/Controller/aluno.js
@@ -1,8 +1,39 @@
 import { openDb } from "../configDB.js";
 
-export async function createTable() {
-    openDb().then(db => {
-        db.exec('CREATE TABLE IF NOT EXISTS Aluno (matricula INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT,cpf INTEGER UNIQUE, email TEXT)');
-    })
+export async function createStudentTable() {
+    openDb()
+        .then(db => {
+            db.exec('CREATE TABLE IF NOT EXISTS Alumni (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,cpf INTEGER UNIQUE, email TEXT)');
+        })
+}
 
+export async function insertStudent(student) {
+    openDb()
+        .then(db => {
+            db.run('INSERT INTO Alumni (name, cpf, email) VALUES (?, ?, ?)', [student.name, student.cpf, student.email]);
+        })
+}
+
+export async function updateStudent(student) {
+    openDb()
+        .then(db => {
+            db.run('UPDATE Alumni SET name = ?, cpf = ?, email = ? WHERE id = ?', [student.name, student.cpf, student.email, student.id]);
+        })
+}
+
+export async function allStudents() {
+    return openDb()
+        .then(db => {
+            return db.all('SELECT * FROM Alumni')
+                .then(students => {
+                    return students;
+                })
+        })
+}
+
+export async function deleteStudent(id) {
+    openDb()
+        .then(db => {
+            db.run('DELETE FROM Alumni WHERE id = ?', [id]);
+        })
 }
